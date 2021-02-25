4532e13ab9613877003375381ffd0a6cdbeffa9e
Smokyo <69753561+Smokyo@users.noreply.github.com>
2021-02-25 23:47:07 +0300

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,9 +1,9 @@
-const ownerId = '778512157926883328'
+Const Discors = require('discord.js');
 module.exports = {
     name: "eval",
     description: "Evaluates a given code",
     run: async(message, args, client) => {
-        if(message.author.id !== ownerId) {
+          if(message.author.id !== '778512157926883328') return;
             return message.reply("Only the bot owner may use this command!")
         }
         let result = eval(args.join(" "))
