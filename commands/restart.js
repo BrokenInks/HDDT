ad19566add0849c3e69d75ee0ef3dc1a4729a568
Smokyo <69753561+Smokyo@users.noreply.github.com>
2021-02-25 23:45:30 +0300

    Update restart.js

--- commands/restart.js
+++ commands/restart.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 module.exports = {
     name: "restart",
     run: async (message, args, client) => {
-        if (message.author.id !== '778512157926883328') {
+          if(message.author.id !== '778512157926883328') return;
             return message.channel.send(`You cannot use this command!`)
         }
         await message.channel.send(`Restarting bot...`)
